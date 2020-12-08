73f9ba5094080f8cd0326f72944ec86c4e76d755
tom-sb <fvillanuevanut@gmail.com>
2020-12-08 15:50:49 -0500

    seventh commit

--- poligono.cpp
+++ poligono.cpp
@@ -34,27 +34,11 @@ double Poligono::getPerimetro(){
 	}
 	return perimetro;
 }
-/*
-Regular::Regular(vector<Punto> puntos){
-	n = puntos.size();
-	vertices = puntos;
-	for(int i=0;i<vertices.size();i++){
-		if(i==n-1){
-			Linea temp(&vertices[i],&vertices[0]);
-			aristas.push_back(temp);
-		}else{
-			Linea temp(&vertices[i],&vertices[i+1]);
-			aristas.push_back(temp);
-		}
-	}
-	for(auto it: aristas){
-		cout<<it.getDistancia()<<endl;
-	}
-}
-*/
+
+#define PI 3.14159265
 double Regular::getArea(){
 	double angulo = 360 /n;
-	double apotema = aristas[0].getDistancia()/ 2*(tan(angulo/2));
+	double apotema = aristas[0].getDistancia()/ 2*(tan((angulo/2)*PI/180));
 	double area;
 	area = (getPerimetro()*apotema)/2;
 	return area;
